98254ad489b06412931ed549ef8f29cbee6832fd
akshar <takleakshar@gmail.com>
2020-02-02 15:59:07 -0800

    personalize - change thumbnail

--- gatsby-config.js
+++ gatsby-config.js
@@ -198,7 +198,7 @@ module.exports = {
         background_color: '#FFF',
         theme_color: '#F7A046',
         display: 'standalone',
-        icon: 'static/akshar.jpg'
+        icon: 'static/avocado.png'
       },
     },
     'gatsby-plugin-offline',
